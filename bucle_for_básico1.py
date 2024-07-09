c6eeaee2fc8796c3bdcbc9568c859d4d77dd95c8
Leandro Buitrago <96851296+LEOBC04@users.noreply.github.com>
2024-07-08 23:15:50 -0500

    main file uploaded

---
+++ bucle_for_básico1.py
@@ -0,0 +1,66 @@
+# Básico1
+
+# for integer in range(101):
+#   print(integer)
+
+  
+# =================================================
+
+# Múltiplos de 2
+
+# for multiples in range (2,501,2):
+#   print(multiples)
+
+
+# =================================================
+
+# Contando Vanilla Ice
+
+# for integer in range(1,100):
+  
+#   if integer % 5 == 0:
+#     if integer % 10 == 0:
+#       # Es divisible por 5 y 10
+#       print("ice ice baby")
+      
+#     else:
+#       # Solo es divisible por 5
+#       print("ice ice")
+    
+#   else:
+#     print(integer)
+
+
+# =================================================
+
+# Wow. Número gigante a la vista
+
+# big_num = 0
+
+# for number in range (500001):
+#   if number % 2 == 0:
+#     big_num += number
+    
+# print(big_num)
+
+
+# =================================================
+
+# Regrésame al 3
+
+# for number in range(2024,0,-3):
+#   print(number)
+
+
+# =================================================
+
+# Contador dinámico
+
+# num_inicial = 3
+# num_final = 10
+# multiplo = 2
+
+# for i in range (num_inicial, num_final+1):
+  
+#   if i % multiplo == 0:
+#     print(i)
